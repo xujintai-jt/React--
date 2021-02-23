ad209f77fac1bc71d3c93a0f5a5eb7a0037a8db2
xujintai-jt <1791639435@qq.com>
2021-02-23 17:40:58 +0800

    react cli项目

--- React_cli/react_cli_test/src/components/App.jsx
+++ React_cli/react_cli_test/src/components/App.jsx
@@ -4,9 +4,12 @@ import logo from '../logo.svg';
 export default function App() {
   return (
     <div className="App-root">
+      {/* 块级元素继承父元素的text-align，行内元素执行父元素的text-align */}
       <img  className="App-img-style" src={logo} alt="logo" />
       <p className="App-p-style">react app组件</p>
       <span>span</span>
+      <hr/>
+      <input type="text" placeholder="我是行内块元素"/>
     </div>
   )
 }
