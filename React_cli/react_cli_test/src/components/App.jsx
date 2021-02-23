05b25583bf2a1398a88471cf2a20dbed0e99c266
xujintai-jt <1791639435@qq.com>
2021-02-23 17:37:57 +0800

    react cli项目

---
+++ React_cli/react_cli_test/src/components/App.jsx
@@ -0,0 +1,18 @@
+import logo from '../logo.svg';
+
+
+export default function App() {
+  return (
+    <div className="App-root">
+      <img  className="App-img-style" src={logo} alt="logo" />
+      <p className="App-p-style">react app组件</p>
+      <span>span</span>
+    </div>
+  )
+}
+
+
+// function App() {
+//   return <img src={logo} alt="logo"/>
+// }
+// export default App;
