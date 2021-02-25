f0d92a37537edb464dd39b9fb769b425e0a9eca6
xujintai-jt <1791639435@qq.com>
2021-02-25 16:56:51 +0800

    react 搜索demo

--- React_cli/react_cli_test/src/components/search/search.jsx
+++ React_cli/react_cli_test/src/components/search/search.jsx
@@ -1,16 +1,44 @@
+import { Component } from "react";
+import axios from 'axios'
 
-import {Component} from 'react'
 export default class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchInfo: "",
+    };
+    //绑定this
+    this.searchInfoChange = this.searchInfoChange.bind(this)
+    this.getInfo = this.getInfo.bind(this)
+  }
 
+  searchInfoChange(event) {
+    const searchInfo=event.target.value
+    this.setState({
+      searchInfo
+    })
+  }
+
+  getInfo() {
+    const { searchInfo } = this.state
+    const url = `https://api.github.com/search/users?=${searchInfo}`
+    axios.get(url).then(Response => {
+      console.log(Response);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 
   render() {
+    const {searchInfo}=this.state
     return (
       <header className="search-header">
         <h2 className="margin-left">Search Github Users</h2>
         <section className="margin-left">
-          <input type="text"/> <button>Search</button>
-       </section>
+          <input type="text" value={searchInfo}  onChange={this.searchInfoChange} />
+          <button onClick={this.getInfo}>Search</button>
+        </section>
       </header>
-    )
+    );
   }
 }
